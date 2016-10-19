5354a604a3fffbbec012fe46274c5aa1baaaa556
stu <stu@qq.com>
2016-10-19 11:35:59 +0800

    123

---
+++ mytest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mytest.Startup))]
+namespace mytest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
